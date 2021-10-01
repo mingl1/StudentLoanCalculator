30ce99faf9e1083f5d374cdb84ee662bac5ed1bd
linm1 <linm1@bxscience.edu>
2021-10-01 09:50:27 -0400

    AppDev Project

--- app/src/main/java/com/example/studentloancalculator/MainActivity.java
+++ app/src/main/java/com/example/studentloancalculator/MainActivity.java
@@ -10,7 +10,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    private EditText loanAmount,interestRate, loanTerm, minimumPayment, loanFees;
+    private EditText loanAmount,interestRate, loanTerm, loanFees;
     private TextView mPayment, totalPayment;
     private Button button;
 
@@ -23,7 +23,6 @@ public class MainActivity extends AppCompatActivity {
         loanAmount = (EditText) findViewById(R.id.editTextNumber);
         interestRate = (EditText) findViewById(R.id.editTextNumber2);
         loanTerm = (EditText) findViewById(R.id.editTextNumber3);
-        minimumPayment = (EditText) findViewById(R.id.editTextNumber4);
         loanFees = (EditText) findViewById(R.id.editTextNumber5);
         mPayment=(TextView)findViewById(R.id.monthly);
         totalPayment=(TextView)findViewById(R.id.total);
@@ -31,25 +30,13 @@ public class MainActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-               // String value1 = loanAmount.getText().toString();
-               // String value2 = interestRate.getText().toString();
-               // String value3 = loanTerm.getText().toString();
-               // String value4 = minimumPayment.getText().toString();
-               // String value5 = loanFees.getText().toString();
-              //  int a = Integer.parseInt(value1);
-             //   int b = Integer.parseInt(value2);
-              //  int c = Integer.parseInt(value3);
-              //  int d = Integer.parseInt(value4);
-              //  int e = Integer.parseInt(value5);
-                int a= Integer.parseInt(loanAmount.getText().toString());
-                int b= Integer.parseInt(interestRate.getText().toString());
-                int c= Integer.parseInt(loanTerm.getText().toString());
-                int d= Integer.parseInt(minimumPayment.getText().toString());
-                int e= Integer.parseInt(loanFees.getText().toString());
-
-
-                int calculation = (a*b/100*c);
-                int calculation2 = (d*b/100*c + e);
+                double a= Integer.parseInt(loanAmount.getText().toString());
+                double b= (Integer.parseInt(interestRate.getText().toString()))/100.0;
+                double c= Integer.parseInt(loanTerm.getText().toString());
+                double e= Integer.parseInt(loanFees.getText().toString());
+                double r=b/12;
+                double calculation = (a+e)/((Math.pow((1+r), (c*12))-1)/(r*Math.pow((1+r),(c*12))));
+                double calculation2 = (calculation*(c*12) -a);
                mPayment.setText(""+calculation);
                totalPayment.setText(""+calculation2);
             }
